1f6d6009ca6f5e2013aeb3f7d892c89f6d38f71b
Abhishek Mathur (mathur17021play@gmail.com) <mathur17021play@gmail.com>
2019-08-15 09:47:03 +0530

    initial push

---
+++ com/android/tools/fd/runtime/AppInfo.java
@@ -0,0 +1,8 @@
+package com.android.tools.fd.runtime;
+
+public class AppInfo {
+    public static String applicationClass = null;
+    public static String applicationId = "com.example.dishaapp";
+    public static long token = 5382568154530578321L;
+    public static boolean usingApkSplits = false;
+}
